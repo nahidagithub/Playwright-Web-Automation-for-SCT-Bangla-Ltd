cda2461486752a757fca48dc7a0936fc79104e0c
nahidagithub <148271590+nahidagithub@users.noreply.github.com>
2026-01-12 16:08:59 +0600

    Update Playwright_Automation.ts

--- Playwright_Automation.ts
+++ Playwright_Automation.ts
@@ -183,7 +183,7 @@ await expect(page.locator('ui5-dialog[open]')).toHaveCount(0);
 
 });
 
-test('TC 09 - Verify clicking any section', async ({ page }) => {
+test('TC 09 - Verify clicking any section from the screen', async ({ page }) => {
 
   await page.goto('http://skt.test.local/en/login');
   await page.locator('ui5-dialog input[name="username"]').fill('nahida');
@@ -290,11 +290,10 @@ await expect(
   page.locator('ui5-illustrated-message')
 ).toBeVisible();
 
-
 });
 
 
-test.only('TC 13 - Verify clicking any record from the screen', async ({ page }) => {
+test('TC 13 - Verify clicking any record from the screen', async ({ page }) => {
 
 await page.goto('http://skt.test.local/en/login');
 await page.locator('ui5-dialog input[name="username"]').fill('nahida');
@@ -314,14 +313,174 @@ await page
   )
   .click();
 
-await page.locator(
-  '#buildDirectoryStructure ui5-tree-item-custom[selected] ui5-icon[aria-label="More"]'
-).click();
+await page.locator('ui5-button[icon="overflow"]').click();
 
-
-
+//Assertion
+await expect(
+  page.locator('ui5-menu').getByText('New Folder')
+).toBeVisible();
 
 });
+
+
+test('TC 14 - Verify clicking new folder button and pop should be opened', async ({ page }) => {
+
+await page.goto('http://skt.test.local/en/login');
+await page.locator('ui5-dialog input[name="username"]').fill('nahida');
+
+
+await page.locator('[name="password"]').fill('1234');
+  //await page.pause();
+
+await page.locator('ui5-dialog.dialog-container').getByRole('button', { name: 'Login', exact: true }).click();
+
+await page.locator('#DocVisu > ui5-card > a').click();
+await page.getByText('Parts', { exact: true }).click();
+
+await page
+  .locator(
+    '.AnalyticalTable_trActive_n66fk_179 div[role="gridcell"][data-column-id-cell="name"]'
+  )
+  .click();
+
+await page.locator('ui5-button[icon="overflow"]').click();
+
+await page.locator('ui5-menu-item[icon="add-folder"]', {
+  hasText: 'New Folder'
+}).click();
+
+//Assertion
+await expect(
+  page.locator('ui5-dialog#isNewFolderDialogOpen')
+).toBeVisible();
+
+});
+
+
+test('TC 15 - Verify adding new folder', async ({ page }) => {
+
+await page.goto('http://skt.test.local/en/login');
+await page.locator('ui5-dialog input[name="username"]').fill('nahida');
+
+
+await page.locator('[name="password"]').fill('1234');
+  //await page.pause();
+
+await page.locator('ui5-dialog.dialog-container').getByRole('button', { name: 'Login', exact: true }).click();
+
+await page.locator('#DocVisu > ui5-card > a').click();
+await page.getByText('Parts', { exact: true }).click();
+
+await page
+  .locator(
+    '.AnalyticalTable_trActive_n66fk_179 div[role="gridcell"][data-column-id-cell="name"]'
+  )
+  .click();
+
+await page.locator('ui5-button[icon="overflow"]').click();
+
+await page.locator('ui5-menu-item[icon="add-folder"]', {
+  hasText: 'New Folder'
+}).click();
+
+await page.locator('ui5-dialog#isNewFolderDialogOpen ui5-input input#inner')
+  .fill('MyNewFolder');
+await page.waitForTimeout(2000)
+
+await page
+  .locator('#isNewFolderDialogOpen')
+  .locator('ui5-button#saveButton')
+  .click();
+
+  //Assertion
+await expect(
+  page.locator('ui5-dialog#isNewFolderDialogOpen')
+).toBeHidden();
+
+});
+
+test.only('TC 16 - Verify clicking three dot button', async ({ page }) => {
+
+await page.goto('http://skt.test.local/en/login');
+await page.locator('ui5-dialog input[name="username"]').fill('nahida');
+
+
+await page.locator('[name="password"]').fill('1234');
+  //await page.pause();
+
+await page.locator('ui5-dialog.dialog-container').getByRole('button', { name: 'Login', exact: true }).click();
+
+await page.locator('#DocVisu > ui5-card > a').click();
+await page.getByText('Parts', { exact: true }).click();
+
+await page
+  .locator(
+    '.AnalyticalTable_trActive_n66fk_179 div[role="gridcell"][data-column-id-cell="name"]'
+  )
+  .click();
+/*
+await page.locator('ui5-button[icon="overflow"]').click();
+
+await page.locator('ui5-menu-item[icon="add-folder"]', {
+  hasText: 'New Folder'
+}).click();
+
+await page.locator('ui5-dialog#isNewFolderDialogOpen ui5-input input#inner')
+  .fill('MyNewFolder');
+await page.waitForTimeout(2000)
+
+await page
+  .locator('#isNewFolderDialogOpen')
+  .locator('ui5-button#saveButton')
+  .click();
+*/
+await page.locator(
+  'ui5-button#menu-23754237540\\.D23754 button[title="More"]'
+).click();
+
+await page.waitForTimeout(2000)
+});
+
+test.only('TC 16 - Verify adding new file', async ({ page }) => {
+
+await page.goto('http://skt.test.local/en/login');
+await page.locator('ui5-dialog input[name="username"]').fill('nahida');
+await page.locator('[name="password"]').fill('1234');
+  //await page.pause();
+
+await page.locator('ui5-dialog.dialog-container').getByRole('button', { name: 'Login', exact: true }).click();
+
+await page.locator('#DocVisu > ui5-card > a').click();
+await page.getByText('Parts', { exact: true }).click();
+
+await page
+  .locator(
+    '.AnalyticalTable_trActive_n66fk_179 div[role="gridcell"][data-column-id-cell="name"]'
+  )
+  .click();
+/*
+await page.locator('ui5-button[icon="overflow"]').click();
+
+await page.locator('ui5-menu-item[icon="add-folder"]', {
+  hasText: 'New Folder'
+}).click();
+
+await page.locator('ui5-dialog#isNewFolderDialogOpen ui5-input input#inner')
+  .fill('MyNewFolder');
+await page.waitForTimeout(2000)
+
+await page
+  .locator('#isNewFolderDialogOpen')
+  .locator('ui5-button#saveButton')
+  .click();
+*/
+await page.locator(
+  'ui5-button#menu-23754237540\\.D23754 button[title="More"]'
+).click();
+
+await page.waitForTimeout(2000)
+});
+
 
 })
 
